8b6497eefee1510a13d6b6bdb22ad7c298079eaf
Alex <a_zolotaryov@ukr.net>
2026-05-08 15:28:52 +0200

    "Cycles"

--- script.js
+++ script.js
@@ -157,18 +157,66 @@
 //     default: console.log('Ne odno');
 //         break
 // }
-const num = 'net';
+// const num = 'net';
 
-switch (typeof num) {
-    case 'string':
-        console.log('DA');
-        break;
-    case 'number':
-        console.log('>Net>');
-        break;
-    case 'boolean':
-        console.log('OK');
-        break;
-    default:
-        console.log('Ne odno');
-}
+// switch (typeof num) {
+//     case 'string':
+//         console.log('DA');
+//         break;
+//     case 'number':
+//         console.log('>Net>');
+//         break;
+//     case 'boolean':
+//         console.log('OK');
+//         break;
+//     default:
+//         console.log('Ne odno');
+// }
+console.log(NaN || 2 || undefined);
+console.log(undefined && 2 && undefined);
+console.log(1 && 2 && 3);
+console.log(!1 || 1);
+console.log(25 || null && !3); console.log(NaN || null || !3 || undefined || 5);
+
+console.log(NaN || null && !3 && undefined || 5);
+console.log(5 === 5 && 3 > 1 || 5);
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// }
+// const hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger || cola || fries === 3 || nuggets) {
+//     console.log('Done!')
+// }
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+
+
+// if (hamburger && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// }
+// let num = 50;
+// while (num < 55) {
+//     console.log(num);
+//    
+// }
+
+let num = 50;
+
+do {
+    num++;
+    console.log(num);
+
+} while (num < 56);
