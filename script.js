79d3cd1187747b7e5008a7173a4f1085fd2eb7b2
Alex <a_zolotaryov@ukr.net>
2026-07-21 23:49:35 +0800

    More drills

--- script.js
+++ script.js
@@ -1136,8 +1136,8 @@
 // // }
 // // console.log(getMathResult(3, 5));
 
-let str = "test";
-console.log(str.toUpperCase());
+// let str = "test";
+// console.log(str.toUpperCase());
 
 // // function getEvevnNum(start, count) {
 // //   if (typeof count !== "number" || count <= 0) {
@@ -1928,17 +1928,17 @@ const student = {
 };
 console.log(student.showStudentInfo(student));
 
-function showSubjectsProgress(person) {
-  const { progress } = person.subjects;
-  let str = "";
-  for (let key in progress) {
-    str += `Предмет ${key} изучен на ${progress[key]}\n`;
-  }
-  return str;
-}
-console.log(person.key);
-console.log(person[key]);
-console.log(showSubjectsProgress(student));
+// function showSubjectsProgress(person) {
+//   const { progress } = person.subjects;
+//   let str = "";
+//   for (let key in progress) {
+//     str += `Предмет ${key} изучен на ${progress[key]}\n`;
+//   }
+//   return str;
+// }
+// console.log(person.key);
+// console.log(person[key]);
+// console.log(showSubjectsProgress(student));
 // 3)
 
 // Добавьте в объект метод showStudentInfo.
@@ -2172,3 +2172,128 @@ console.log(showTechnologies(company));
 
 // test(company);
 // console.log(company.name);
+
+const lines = 5;
+let result = "";
+// Проверяется именно переменная result, формируйте строку в ней
+for (let row = 0; row <= lines; row++) {
+  for (let space = 0; space < lines - row; space++) {
+    result += " ";
+  }
+  for (let stars = 0; stars < 2 * row + 1; stars++) {
+    result += "*";
+  }
+  result += "\n";
+}
+console.log(result);
+
+//Coding Exercise 11: Задачи на работу с массивами========================================================================================================================================================
+//Задачи:
+// 1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+// showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+// Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение ''
+const family = ["Peter", "Ann", "Alex", "Linda"];
+
+function showFamily(arr) {
+  let result = "Семья состоит из: ";
+  if (arr.length === 0) {
+    return `Семья пуста`;
+  }
+  for (let member of arr) {
+    result += `${member} `;
+  }
+  return result.trim();
+}
+console.log(showFamily(family));
+
+// 2) напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+
+// Пример:
+
+// standardizeStrings(favoriteCities)  выведет в консоль
+
+// lisbon
+// rome
+// milan
+// dublin
+const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+function standardizeStrings(arr) {
+  for (let city of arr) {
+    console.log(city.toLowerCase());
+  }
+}
+standardizeStrings(favoriteCities);
+
+//VANYA========================================================================================================================================================
+const family = ["Peter", "Ann", "Alex", "Linda"];
+
+// function showFamily(arr) {
+//   let str = "";
+
+//   arr.length === 0 ? (str = "Семья пуста") : (str = "Семья состоит из: ");
+
+//   arr.forEach((member) => {
+//     str += `${member} `;
+//   });
+
+//   return str;
+// }
+
+showFamily(family);
+
+const favoriteCities = ["liSBon", "ROME", "miLan", "Dublin"];
+
+function standardizeStrings(arr) {
+  arr.forEach((city) => {
+    console.log(city.toLowerCase());
+  });
+}
+
+standardizeStrings(favoriteCities);
+
+//Coding Exercise 12: Задачи на работу с массивами, часть 2========================================================================================================================================================
+// 3) Задача с собеседований. Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.
+
+// Пример:
+// const someString = 'This is some strange string';
+// reverse(someString) => 'gnirts egnarts emos si sihT'
+
+// Функцию можно применить к любой строке. Если в функцию приходит не строка - вернуть сообщение "Ошибка!"
+const someString = "This is some strange string";
+
+function reverse(str) {
+  if (typeof str !== "string") {
+    return `Ошибка!`;
+  } else {
+    let someArr = str.split("");
+    str = someArr.reverse();
+    str = someArr.join("");
+  }
+
+  return str;
+}
+reverse(someString);
+
+//========================================================================================================================================================
+// 4) Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
+// Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу (сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде.
+// Пример:
+// availableCurr(['UAH', 'RUB', 'CNY'], 'CNY')
+// Вернет строку:
+// Доступные валюты:
+// UAH
+// RUB
+// Заметьте:
+// - CNY (юань) исчез из списка валют, значит такая валюта закончилась
+// - После валюты: стоит перенос строки \n, и после каждой валюты тоже. Это важно для тестов
+// - Данные для первого аргумента должны приходить сразу из двух банков, причем сначала baseCurrencies, потом additionalCurrencies по порядку
+const baseCurrencies = ["USD", "EUR"];
+const additionalCurrencies = ["UAH", "RUB", "CNY"];
+
+function availableCurr(arr, missingCurr) {
+  let result = "";
+  if (arr.length === 0) {
+    return `Нет доступных валют`;
+  } else result += `Доступные валюты: `;
+}
