5fe683c8f67731bd54a4e9de19eb2c9c90b6bb9f
Alex <a_zolotaryov@ukr.net>
2026-09-24 22:23:42 +0800

    Checked done

--- Movies_Project/js/script.js
+++ Movies_Project/js/script.js
@@ -12,6 +12,23 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
 "use strict";
 document.addEventListener("DOMContentLoaded", () => {
   const movieDB = {
@@ -20,6 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
       "Лига справедливости",
       "Ла-ла лэнд",
       "Одержимость",
+      "Пилигрим против...",
       "Скотт Пилигрим против...",
     ],
   };
@@ -27,8 +45,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const adv = document.querySelectorAll(".promo__adv img"),
     poster = document.querySelector(".promo__bg"),
     genre = poster.querySelector(".promo__genre"),
-    movieList = document.querySelector(".promo__interactive-list");
+    movieList = document.querySelector(".promo__interactive-list"),
+    form = document.querySelector("form.add"),
+    input = form.querySelector(".adding__input"),
+    checkbox = form.querySelector("[type='checkbox']");
 
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const newFilm = input.value;
+    const checked = checkbox.checked;
+    movieDB.movies.push(newFilm);
+    movieDB.movies.sort();
+    if (checked) {
+      console.log("Сделать любимым");
+    }
+  });
   adv.forEach((item) => {
     item.remove();
   });
@@ -43,7 +74,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   movieDB.movies.forEach((film, i) => {
     movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1} ${film}
+        <li class="promo__interactive-item">${i + 1}. ${film}
             <div class="delete"></div>
         </li>
     `;
